8c1943789099aba451b8140efb9cee65e6787631
Formanek Dénes <formdenes@gmail.com>
2022-05-13 15:53:04 +0200

    Google sign in working

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,12 +10,10 @@ import { HomePageComponent } from './home-page/home-page.component';
 
 import { environment } from 'src/environments/environment';
 
-import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
-import { provideAuth,getAuth } from '@angular/fire/auth';
-// import { provideDatabase,getDatabase } from '@angular/fire/database';
-import { provideFirestore,getFirestore } from '@angular/fire/firestore';
-// import { provideFunctions,getFunctions } from '@angular/fire/functions';
-// import { provideStorage,getStorage } from '@angular/fire/storage';
+import { AngularFireModule } from '@angular/fire/compat';
+import { AngularFirestoreModule } from '@angular/fire/compat/firestore/';
+import { AngularFireAuthModule } from '@angular/fire/compat/auth';
+
 
 @NgModule({
   declarations: [
@@ -27,12 +25,9 @@ import { provideFirestore,getFirestore } from '@angular/fire/firestore';
     AppRoutingModule,
     BrowserAnimationsModule,
     SharedModule,
-    provideFirebaseApp(() => initializeApp(environment.firebase)),
-    provideAuth(() => getAuth()),
-    // provideDatabase(() => getDatabase()),
-    provideFirestore(() => getFirestore()),
-    // provideFunctions(() => getFunctions()),
-    // provideStorage(() => getStorage())
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFirestoreModule,
+    AngularFireAuthModule
   ],
   providers: [],
   bootstrap: [AppComponent]
