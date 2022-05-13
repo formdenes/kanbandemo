606bc77443c1a9746792c6c937981f048d04fa96
Formanek Dénes <formdenes@gmail.com>
2022-05-13 18:18:42 +0200

    auth guard + logout button when logged in

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomePageComponent } from './home-page/home-page.component';
+import { AuthGuard } from './user/auth.guard';
 
 const routes: Routes = [
   {path: '', component: HomePageComponent},
