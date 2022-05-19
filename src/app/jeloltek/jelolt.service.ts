b4b816627e81d10d48f63525d01d5021d02f3d81
Formanek Dénes (DXC) <dformanek@dxc.com>
2022-05-19 11:18:40 +0200

    created addJelolt service

--- src/app/jeloltek/jelolt.service.ts
+++ src/app/jeloltek/jelolt.service.ts
@@ -1,9 +1,19 @@
 import { Injectable } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
+import { AngularFirestore } from '@angular/fire/compat/firestore';
+import firebase from 'firebase/compat/app';
+import { switchMap, map } from 'rxjs/operators';
+import { Jelolt } from './jelolt.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class JeloltService {
 
-  constructor() { }
+  constructor(private afAuth: AngularFireAuth, private db: AngularFirestore) { }
+
+  async addJelolt(data: Jelolt) {
+    const user = await this.afAuth.currentUser;
+    return this.db.collection('jeloltek').add({...data})
+  }
 }
