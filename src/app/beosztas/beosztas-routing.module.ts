9b1a0c0b59de2cb4f4bbe8a39d79b7b1265ec763
Formanek Dénes (DXC) <dformanek@dxc.com>
2022-05-23 11:10:20 +0200

    updated routing module

--- src/app/beosztas/beosztas-routing.module.ts
+++ src/app/beosztas/beosztas-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BeosztasComponent } from './beosztas/beosztas.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: BeosztasComponent },
+  {
+    path: 'Beosztas/:stage',
+    component: BeosztasComponent
+  }];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- src/app/beosztas/beosztas.module.ts
+++ src/app/beosztas/beosztas.module.ts
@@ -5,10 +5,13 @@ import { BeosztasRoutingModule } from './beosztas-routing.module';
 import { SharedModule } from '../shared/shared.module';
 import { FormsModule } from '@angular/forms';
 import { DragDropModule } from '@angular/cdk/drag-drop';
+import { BeosztasComponent } from './beosztas/beosztas.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    BeosztasComponent
+  ],
   imports: [
     CommonModule,
     BeosztasRoutingModule,
